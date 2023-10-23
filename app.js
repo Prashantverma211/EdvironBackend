805a2f345abafacc47846d4e1c41c761aaec6b11
PRASHANT VERMA <66440207+Prashantverma211@users.noreply.github.com>
2023-10-23 23:38:57 +0530

    change

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With,Content-Type, Accept,Authorization,"
+  );
+  if (req.method === "OPTIONS") {
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST,PUT,DELETE,OPTIONS"
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use("/", form);
 
 app.use((req, res, next) => {
